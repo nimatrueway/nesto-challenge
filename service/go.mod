e12873b7806d0b024be8e09096fa3681d7a10482
Naeim Taheri <nima.trueway@gmail.com>
2024-06-09 21:34:26 -0700

    test: unit test repository

--- service/go.mod
+++ service/go.mod
@@ -6,10 +6,13 @@ require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-playground/validator/v10 v10.20.0
 	github.com/lmittmann/tint v1.0.4
+	github.com/mattn/go-sqlite3 v1.14.17
 	github.com/orandin/slog-gorm v1.3.2
 	github.com/samber/slog-gin v1.13.3
 	github.com/spf13/viper v1.19.0
+	github.com/stretchr/testify v1.9.0
 	gorm.io/driver/postgres v1.5.7
+	gorm.io/driver/sqlite v1.5.5
 	gorm.io/gorm v1.25.10
 )
 
@@ -18,6 +21,7 @@ require (
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -41,6 +45,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -58,7 +63,7 @@ require (
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
---
+++ service/internal/repository/repository_test.go
@@ -0,0 +1,301 @@
+package repository
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+	"readcommend/internal/repository/model"
+	"testing"
+)
+
+func TestGetBooks(t *testing.T) {
+	db := createSqliteDB(t)
+	seed(db)
+
+	// create repository
+	repository := NewBookRepository(db)
+
+	t.Run("get all the books", func(t *testing.T) {
+		books, err := repository.GetBooks(nil, nil, 0, 0, 0, 0, 0)
+		assert.NoError(t, err)
+		assert.Len(t, books, 58)
+
+		assert.Equal(t, books[0], model.Book{ID: 12,
+			Title:         "Can I Be Honest?",
+			YearPublished: 2007,
+			Rating:        4.77,
+			Pages:         542,
+			GenreID:       1,
+			AuthorID:      11,
+			Genre: model.Genre{
+				ID:    1,
+				Title: "Young Adult",
+			},
+			Author: model.Author{
+				ID:        11,
+				FirstName: "Charles",
+				LastName:  "Fenimore",
+			},
+		})
+	})
+
+	t.Run("single conditional", func(t *testing.T) {
+		t.Run("get books of author #1", func(t *testing.T) {
+			books, err := repository.GetBooks([]int{1}, nil, 0, 0, 0, 0, 0)
+			assert.NoError(t, err)
+			assert.Len(t, books, 1)
+		})
+
+		t.Run("get books of author #4", func(t *testing.T) {
+			books, err := repository.GetBooks([]int{4}, nil, 0, 0, 0, 0, 0)
+			assert.NoError(t, err)
+			assert.Len(t, books, 2)
+		})
+
+		t.Run("get books of author #1 and #4", func(t *testing.T) {
+			books, err := repository.GetBooks([]int{1, 4}, nil, 0, 0, 0, 0, 0)
+			assert.NoError(t, err)
+			assert.Len(t, books, 3)
+		})
+
+		t.Run("get books of genre #1 and #2", func(t *testing.T) {
+			books, err := repository.GetBooks(nil, []int{1, 2}, 0, 0, 0, 0, 0)
+			assert.NoError(t, err)
+			assert.Len(t, books, 6+11)
+		})
+
+		t.Run("get books of min-page 1000", func(t *testing.T) {
+			books, err := repository.GetBooks(nil, nil, 875, 0, 0, 0, 0)
+			assert.NoError(t, err)
+			assert.Len(t, books, 2)
+		})
+
+		t.Run("get books of max-page 10", func(t *testing.T) {
+			books, err := repository.GetBooks(nil, nil, 0, 50, 0, 0, 0)
+			assert.NoError(t, err)
+			assert.Len(t, books, 6)
+		})
+
+		t.Run("get books of min-year 2020", func(t *testing.T) {
+			books, err := repository.GetBooks(nil, nil, 0, 0, 2020, 0, 0)
+			assert.NoError(t, err)
+			assert.Len(t, books, 2)
+		})
+
+		t.Run("get books of max-year 10", func(t *testing.T) {
+			books, err := repository.GetBooks(nil, nil, 0, 0, 0, 1930, 0)
+			assert.NoError(t, err)
+			assert.Len(t, books, 1)
+		})
+
+		t.Run("get a maximum of 5 books of all available", func(t *testing.T) {
+			books, err := repository.GetBooks(nil, nil, 0, 0, 0, 0, 5)
+			assert.NoError(t, err)
+			assert.Len(t, books, 5)
+		})
+	})
+
+	t.Run("multi conditional", func(t *testing.T) {
+		t.Run("get books of books of author #1 and #4 intersect with genre #1 and #2", func(t *testing.T) {
+			books, err := repository.GetBooks([]int{1, 4}, []int{1, 2}, 0, 0, 0, 0, 0)
+			assert.NoError(t, err)
+			assert.Len(t, books, 1)
+		})
+
+		t.Run("get books of min-year 2000 limited to 5", func(t *testing.T) {
+			books, err := repository.GetBooks(nil, nil, 0, 0, 2000, 0, 5)
+			assert.NoError(t, err)
+			assert.Len(t, books, 5)
+			for _, book := range books {
+				assert.GreaterOrEqual(t, book.YearPublished, 2000)
+			}
+		})
+	})
+}
+
+func TestGetAuthors(t *testing.T) {
+	db := createSqliteDB(t)
+	seed(db)
+
+	// create repository
+	repository := NewBookRepository(db)
+
+	// get all authors
+	authors, err := repository.GetAuthors()
+	assert.NoError(t, err)
+	assert.Len(t, authors, 41)
+}
+
+func TestGetGenres(t *testing.T) {
+	db := createSqliteDB(t)
+	seed(db)
+
+	// create repository
+	repository := NewBookRepository(db)
+
+	// get all genres
+	genres, err := repository.GetGenres()
+	assert.NoError(t, err)
+	assert.Len(t, genres, 8)
+}
+
+func TestGetSizes(t *testing.T) {
+	db := createSqliteDB(t)
+	seed(db)
+
+	// create repository
+	repository := NewBookRepository(db)
+
+	// get all sizes
+	sizes, err := repository.GetSizes()
+	assert.NoError(t, err)
+	assert.Len(t, sizes, 7)
+}
+
+func TestGetEras(t *testing.T) {
+	db := createSqliteDB(t)
+	seed(db)
+
+	// create repository
+	repository := NewBookRepository(db)
+
+	// get all eras
+	eras, err := repository.GetEras()
+	assert.NoError(t, err)
+	assert.Len(t, eras, 3)
+}
+
+func createSqliteDB(t *testing.T) *gorm.DB {
+	db, err := gorm.Open(sqlite.Open(":memory:?cache=shared"), &gorm.Config{NamingStrategy: schema.NamingStrategy{SingularTable: true}})
+	assert.NoError(t, err)
+	err = db.AutoMigrate(model.Author{}, model.Era{}, model.Genre{}, model.Size{}, model.Book{})
+	assert.NoError(t, err)
+	return db
+}
+
+func seed(db *gorm.DB) {
+	db.Create(&model.Era{ID: 1, Title: "Any"})
+	db.Create(&model.Era{ID: 2, Title: "Classic", MaxYear: sql.NullInt16{Int16: 1969, Valid: true}})
+	db.Create(&model.Era{ID: 3, Title: "Modern", MinYear: sql.NullInt16{Int16: 1970, Valid: true}})
+
+	db.Create(&model.Size{ID: 1, Title: "Any"})
+	db.Create(&model.Size{ID: 2, Title: "Short story – up to 35 pages", MaxPages: sql.NullInt16{Int16: 34, Valid: true}})
+	db.Create(&model.Size{ID: 3, Title: "Novelette – 35 to 85 pages", MinPages: sql.NullInt16{Int16: 35, Valid: true}, MaxPages: sql.NullInt16{Int16: 84, Valid: true}})
+	db.Create(&model.Size{ID: 4, Title: "Novella – 85 to 200 pages", MinPages: sql.NullInt16{Int16: 85, Valid: true}, MaxPages: sql.NullInt16{Int16: 199, Valid: true}})
+	db.Create(&model.Size{ID: 5, Title: "Novel – 200 to 500 pages", MinPages: sql.NullInt16{Int16: 200, Valid: true}, MaxPages: sql.NullInt16{Int16: 499, Valid: true}})
+	db.Create(&model.Size{ID: 6, Title: "Brick – 500 to 800 pages", MinPages: sql.NullInt16{Int16: 500, Valid: true}, MaxPages: sql.NullInt16{Int16: 799, Valid: true}})
+	db.Create(&model.Size{ID: 7, Title: "Monument – 800 pages and up", MinPages: sql.NullInt16{Int16: 800, Valid: true}})
+
+	db.Create(&model.Genre{ID: 1, Title: "Young Adult"})
+	db.Create(&model.Genre{ID: 2, Title: "SciFi/Fantasy"})
+	db.Create(&model.Genre{ID: 3, Title: "Romance"})
+	db.Create(&model.Genre{ID: 4, Title: "Nonfiction"})
+	db.Create(&model.Genre{ID: 5, Title: "Mystery"})
+	db.Create(&model.Genre{ID: 6, Title: "Memoir"})
+	db.Create(&model.Genre{ID: 7, Title: "Fiction"})
+	db.Create(&model.Genre{ID: 8, Title: "Childrens"})
+
+	db.Create(&model.Author{ID: 1, FirstName: "Wendell", LastName: "Stackhouse"})
+	db.Create(&model.Author{ID: 2, FirstName: "Amelia", LastName: "Wangerin, Jr."})
+	db.Create(&model.Author{ID: 3, FirstName: "Anastasia", LastName: "Inez"})
+	db.Create(&model.Author{ID: 4, FirstName: "Arthur", LastName: "McCrumb"})
+	db.Create(&model.Author{ID: 5, FirstName: "Arturo", LastName: "Hijuelos"})
+	db.Create(&model.Author{ID: 6, FirstName: "Bernard", LastName: "Hopf"})
+	db.Create(&model.Author{ID: 7, FirstName: "Bianca", LastName: "Thompson"})
+	db.Create(&model.Author{ID: 8, FirstName: "Bravig", LastName: "Lewisohn"})
+	db.Create(&model.Author{ID: 9, FirstName: "Burton", LastName: "Malamud"})
+	db.Create(&model.Author{ID: 10, FirstName: "Carolyn", LastName: "Segal"})
+	db.Create(&model.Author{ID: 11, FirstName: "Charles", LastName: "Fenimore"})
+	db.Create(&model.Author{ID: 12, FirstName: "Clifford", LastName: "Wolitzer"})
+	db.Create(&model.Author{ID: 13, FirstName: "Darryl", LastName: "Fleischman"})
+	db.Create(&model.Author{ID: 14, FirstName: "David", LastName: "Beam"})
+	db.Create(&model.Author{ID: 15, FirstName: "Elizabeth", LastName: "Herbach"})
+	db.Create(&model.Author{ID: 16, FirstName: "Elmer", LastName: "Komroff"})
+	db.Create(&model.Author{ID: 17, FirstName: "Gloria", LastName: "Green"})
+	db.Create(&model.Author{ID: 18, FirstName: "Grace", LastName: "Harrison"})
+	db.Create(&model.Author{ID: 19, FirstName: "Hamlin", LastName: "Myrer"})
+	db.Create(&model.Author{ID: 20, FirstName: "Hillary", LastName: "Barnhardt"})
+	db.Create(&model.Author{ID: 21, FirstName: "Jill", LastName: "Hergesheimer"})
+	db.Create(&model.Author{ID: 22, FirstName: "John W.", LastName: "Spanogle"})
+	db.Create(&model.Author{ID: 23, FirstName: "Jonathan", LastName: "Kotzwinkle"})
+	db.Create(&model.Author{ID: 24, FirstName: "Kathy", LastName: "Yglesias"})
+	db.Create(&model.Author{ID: 25, FirstName: "Kenneth", LastName: "Douglas"})
+	db.Create(&model.Author{ID: 26, FirstName: "Kris", LastName: "Elegant"})
+	db.Create(&model.Author{ID: 27, FirstName: "Langston", LastName: "Lippman"})
+	db.Create(&model.Author{ID: 28, FirstName: "Leonard", LastName: "Nabokov"})
+	db.Create(&model.Author{ID: 29, FirstName: "Lori", LastName: "Kaan"})
+	db.Create(&model.Author{ID: 30, FirstName: "Lynne", LastName: "Danticat"})
+	db.Create(&model.Author{ID: 31, FirstName: "Malin", LastName: "Wolff"})
+	db.Create(&model.Author{ID: 32, FirstName: "Oliver", LastName: "Lowry"})
+	db.Create(&model.Author{ID: 33, FirstName: "Patricia", LastName: "Hazzard"})
+	db.Create(&model.Author{ID: 34, FirstName: "Philip", LastName: "Antrim"})
+	db.Create(&model.Author{ID: 35, FirstName: "Phoebe", LastName: "Brown"})
+	db.Create(&model.Author{ID: 36, FirstName: "R.M.", LastName: "Larner"})
+	db.Create(&model.Author{ID: 37, FirstName: "Robert", LastName: "Plimpton"})
+	db.Create(&model.Author{ID: 38, FirstName: "Robert", LastName: "Milofsky"})
+	db.Create(&model.Author{ID: 39, FirstName: "Ursula", LastName: "Karénine"})
+	db.Create(&model.Author{ID: 40, FirstName: "Ward", LastName: "Haigh"})
+	db.Create(&model.Author{ID: 41, FirstName: "Abraham", LastName: "Barton"})
+
+	db.Create(&model.Book{ID: 1, Title: "Alanna Saves the Day", YearPublished: 1972, Rating: 1.62, Pages: 169, GenreID: 8, AuthorID: 6})
+	db.Create(&model.Book{ID: 2, Title: "Adventures of Kaya", YearPublished: 1999, Rating: 2.13, Pages: 619, GenreID: 1, AuthorID: 40})
+	db.Create(&model.Book{ID: 3, Title: "A Horrible Human with the Habits of a Monster", YearPublished: 1976, Rating: 1.14, Pages: 258, GenreID: 7, AuthorID: 25})
+	db.Create(&model.Book{ID: 4, Title: "And I Said Yes", YearPublished: 1954, Rating: 3.3, Pages: 183, GenreID: 7, AuthorID: 16})
+	db.Create(&model.Book{ID: 5, Title: "Ballinby Boys", YearPublished: 1960, Rating: 1.88, Pages: 205, GenreID: 2, AuthorID: 4})
+	db.Create(&model.Book{ID: 6, Title: "Banana Slug and the Lost Cow", YearPublished: 1983, Rating: 2.53, Pages: 527, GenreID: 8, AuthorID: 20})
+	db.Create(&model.Book{ID: 7, Title: "Banana Slug and Xyr Friends", YearPublished: 1989, Rating: 3.64, Pages: 558, GenreID: 8, AuthorID: 20})
+	db.Create(&model.Book{ID: 8, Title: "Banana Slug and the Glass Half Full", YearPublished: 1952, Rating: 4.51, Pages: 796, GenreID: 8, AuthorID: 17})
+	db.Create(&model.Book{ID: 9, Title: "Banana Slug and the Mossy Rock", YearPublished: 2006, Rating: 4.43, Pages: 70, GenreID: 8, AuthorID: 31})
+	db.Create(&model.Book{ID: 10, Title: "Burnished Silver", YearPublished: 1932, Rating: 1.2, Pages: 202, GenreID: 3, AuthorID: 30})
+	db.Create(&model.Book{ID: 11, Title: "Cimornul", YearPublished: 1942, Rating: 1.08, Pages: 791, GenreID: 2, AuthorID: 21})
+	db.Create(&model.Book{ID: 12, Title: "Can I Be Honest?", YearPublished: 2007, Rating: 4.77, Pages: 542, GenreID: 1, AuthorID: 11})
+	db.Create(&model.Book{ID: 13, Title: "Concerning Prophecy", YearPublished: 1944, Rating: 3.8, Pages: 155, GenreID: 2, AuthorID: 18})
+	db.Create(&model.Book{ID: 14, Title: "Don't Check your Ego", YearPublished: 1993, Rating: 3.02, Pages: 100, GenreID: 4, AuthorID: 36})
+	db.Create(&model.Book{ID: 15, Title: "The Deep Grey", YearPublished: 1931, Rating: 3.94, Pages: 43, GenreID: 7, AuthorID: 37})
+	db.Create(&model.Book{ID: 16, Title: "Dust on the Rim", YearPublished: 1946, Rating: 4.24, Pages: 38, GenreID: 2, AuthorID: 24})
+	db.Create(&model.Book{ID: 17, Title: "Did You Hear?", YearPublished: 1954, Rating: 2.48, Pages: 887, GenreID: 7, AuthorID: 30})
+	db.Create(&model.Book{ID: 18, Title: "Heliotrope Pajamas", YearPublished: 1952, Rating: 3.74, Pages: 16, GenreID: 8, AuthorID: 31})
+	db.Create(&model.Book{ID: 19, Title: "Hashtag QuokkaSelfie", YearPublished: 1995, Rating: 3.42, Pages: 417, GenreID: 4, AuthorID: 27})
+	db.Create(&model.Book{ID: 20, Title: "Interrobangs for All", YearPublished: 2011, Rating: 3.37, Pages: 677, GenreID: 7, AuthorID: 16})
+	db.Create(&model.Book{ID: 21, Title: "Inconvenient Confessions: a 6", YearPublished: 1972, Rating: 4.11, Pages: 766, GenreID: 6, AuthorID: 32})
+	db.Create(&model.Book{ID: 22, Title: "It's Never Just a Glass", YearPublished: 1956, Rating: 3.55, Pages: 305, GenreID: 1, AuthorID: 28})
+	db.Create(&model.Book{ID: 23, Title: "Kalakalal Avenue", YearPublished: 2016, Rating: 4.27, Pages: 26, GenreID: 7, AuthorID: 16})
+	db.Create(&model.Book{ID: 24, Title: "Lace and Brandy", YearPublished: 1967, Rating: 4.13, Pages: 158, GenreID: 3, AuthorID: 30})
+	db.Create(&model.Book{ID: 25, Title: "Land Water Sky Space", YearPublished: 1983, Rating: 1.64, Pages: 320, GenreID: 4, AuthorID: 15})
+	db.Create(&model.Book{ID: 26, Title: "(im)Mortality", YearPublished: 1985, Rating: 1.72, Pages: 214, GenreID: 1, AuthorID: 12})
+	db.Create(&model.Book{ID: 27, Title: "Muddy Waters", YearPublished: 2020, Rating: 4.76, Pages: 594, GenreID: 3, AuthorID: 30})
+	db.Create(&model.Book{ID: 28, Title: "Not to Gossip, But", YearPublished: 1958, Rating: 3.96, Pages: 537, GenreID: 7, AuthorID: 17})
+	db.Create(&model.Book{ID: 29, Title: "Nothing But Capers", YearPublished: 2004, Rating: 3.87, Pages: 347, GenreID: 4, AuthorID: 1})
+	db.Create(&model.Book{ID: 30, Title: "No More Lightning", YearPublished: 1978, Rating: 3.16, Pages: 99, GenreID: 7, AuthorID: 11})
+	db.Create(&model.Book{ID: 31, Title: "Natural Pamplemousse", YearPublished: 1957, Rating: 4.66, Pages: 886, GenreID: 4, AuthorID: 35})
+	db.Create(&model.Book{ID: 32, Title: "9803 North Millworks Road", YearPublished: 1935, Rating: 4.76, Pages: 449, GenreID: 5, AuthorID: 10})
+	db.Create(&model.Book{ID: 33, Title: "Post Alley", YearPublished: 2014, Rating: 1.63, Pages: 374, GenreID: 7, AuthorID: 9})
+	db.Create(&model.Book{ID: 34, Title: "Portmeirion", YearPublished: 2020, Rating: 2.11, Pages: 277, GenreID: 2, AuthorID: 7})
+	db.Create(&model.Book{ID: 35, Title: "Quiddity and Quoddity", YearPublished: 2005, Rating: 2.42, Pages: 318, GenreID: 1, AuthorID: 21})
+	db.Create(&model.Book{ID: 36, Title: "Rystwyth", YearPublished: 1930, Rating: 1.6, Pages: 59, GenreID: 2, AuthorID: 7})
+	db.Create(&model.Book{ID: 37, Title: "Saint Esme", YearPublished: 1949, Rating: 1.84, Pages: 196, GenreID: 3, AuthorID: 30})
+	db.Create(&model.Book{ID: 38, Title: "Some Eggs or Something?", YearPublished: 1997, Rating: 3.24, Pages: 12, GenreID: 7, AuthorID: 29})
+	db.Create(&model.Book{ID: 39, Title: "Say it with Snap!", YearPublished: 1989, Rating: 3.77, Pages: 499, GenreID: 4, AuthorID: 22})
+	db.Create(&model.Book{ID: 40, Title: "Soft, Pliable Truth", YearPublished: 1933, Rating: 3.28, Pages: 453, GenreID: 2, AuthorID: 38})
+	db.Create(&model.Book{ID: 41, Title: "She Also Tottered", YearPublished: 2010, Rating: 2.09, Pages: 225, GenreID: 2, AuthorID: 38})
+	db.Create(&model.Book{ID: 42, Title: "The Spark and The Ashes", YearPublished: 2000, Rating: 2.71, Pages: 721, GenreID: 1, AuthorID: 39})
+	db.Create(&model.Book{ID: 43, Title: "Thatchwork Cottage", YearPublished: 1986, Rating: 2.43, Pages: 667, GenreID: 7, AuthorID: 9})
+	db.Create(&model.Book{ID: 44, Title: "Tales of the Compass", YearPublished: 1945, Rating: 4.22, Pages: 570, GenreID: 2, AuthorID: 24})
+	db.Create(&model.Book{ID: 45, Title: "The Elephant House", YearPublished: 1979, Rating: 3.95, Pages: 349, GenreID: 4, AuthorID: 22})
+	db.Create(&model.Book{ID: 46, Title: "The Winchcombe Railway Museum Heist", YearPublished: 2004, Rating: 3.04, Pages: 731, GenreID: 5, AuthorID: 10})
+	db.Create(&model.Book{ID: 47, Title: "The Startling End of Mr. Hidhoo", YearPublished: 1986, Rating: 1.59, Pages: 842, GenreID: 7, AuthorID: 23})
+	db.Create(&model.Book{ID: 48, Title: "The Thing Is", YearPublished: 1988, Rating: 2.83, Pages: 115, GenreID: 7, AuthorID: 17})
+	db.Create(&model.Book{ID: 49, Title: "The Mallemaroking", YearPublished: 1970, Rating: 1.95, Pages: 418, GenreID: 2, AuthorID: 7})
+	db.Create(&model.Book{ID: 50, Title: "The Scent of Oranges", YearPublished: 2006, Rating: 2.37, Pages: 264, GenreID: 3, AuthorID: 30})
+	db.Create(&model.Book{ID: 51, Title: "the life and times of an utterly inconsequential person", YearPublished: 1992, Rating: 1, Pages: 509, GenreID: 7, AuthorID: 4})
+	db.Create(&model.Book{ID: 52, Title: "The Seawitch Sings", YearPublished: 1977, Rating: 4.62, Pages: 90, GenreID: 3, AuthorID: 30})
+	db.Create(&model.Book{ID: 53, Title: "Turn Left Til You Get There", YearPublished: 1985, Rating: 4.54, Pages: 331, GenreID: 7, AuthorID: 26})
+	db.Create(&model.Book{ID: 54, Title: "The Triscanipt", YearPublished: 2018, Rating: 2.26, Pages: 16, GenreID: 2, AuthorID: 39})
+	db.Create(&model.Book{ID: 55, Title: "Whither Thou Goest", YearPublished: 1963, Rating: 4.44, Pages: 146, GenreID: 3, AuthorID: 30})
+	db.Create(&model.Book{ID: 56, Title: "Who Did You Think You Were Kidding?", YearPublished: 1986, Rating: 4.6, Pages: 867, GenreID: 6, AuthorID: 34})
+	db.Create(&model.Book{ID: 57, Title: "We're Sisters and We Kinda Like Each Other", YearPublished: 1989, Rating: 4.71, Pages: 67, GenreID: 6, AuthorID: 33})
+	db.Create(&model.Book{ID: 58, Title: "Zero over Twelve", YearPublished: 1981, Rating: 1.01, Pages: 287, GenreID: 5, AuthorID: 9})
+}
